43ccbc09efb9335c5150919e877345520e2807fc
Peter-TMK <tundepeter01@gmail.com>
2024-04-29 15:36:59 +0100

    CRUD implementation and refactoring

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -230,7 +230,7 @@ const Note = require("../models/Notes");
 // };
 
 exports.dashboard = async (req, res) => {
-  let perPage = 12;
+  let perPage = 8;
   let page = req.query.page || 1;
 
   const locals = {
@@ -241,12 +241,12 @@ exports.dashboard = async (req, res) => {
   try {
     // Mongoose "^7.0.0 Update
     const notes = await Note.aggregate([
-      { $sort: { updatedAt: -1 } },
+      { $sort: { createdAt: -1 } },
       { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
       {
         $project: {
           title: { $substr: ["$title", 0, 30] },
-          body: { $substr: ["$body", 0, 100] },
+          body: { $substr: ["$body", 0, 200] },
         },
       },
     ])
@@ -263,6 +263,93 @@ exports.dashboard = async (req, res) => {
       layout: "../views/layouts/dashboard",
       current: page,
       pages: Math.ceil(count / perPage),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.dashboardViewNote = async (req, res) => {
+  const note = await Note.findById({ _id: req.params.id })
+    .where({ user: req.user.id })
+    .lean();
+
+  if (note) {
+    res.render("dashboard/view-note", {
+      noteID: req.params.id,
+      note,
+      layout: "../views/layouts/dashboard",
+    });
+  } else {
+    res.send("Something went wrong");
+  }
+};
+
+exports.dashboardUpdateNote = async (req, res) => {
+  try {
+    await Note.findOneAndUpdate(
+      { _id: req.params.id },
+      { title: req.body.title, body: req.body.body, createdAt: Date.now() }
+    ).where({ user: req.user.id });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.dashboardDeleteNote = async (req, res) => {
+  try {
+    await Note.deleteOne({ _id: req.params.id }).where({
+      user: req.user.id,
+    });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.dashboardAddNote = async (req, res) => {
+  res.render("dashboard/add", {
+    layout: "../views/layouts/dashboard",
+  });
+};
+
+exports.dashboardSubmitNote = async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Note.create(req.body);
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.dashboardSearch = async (req, res) => {
+  try {
+    res.render("dashboard/search", {
+      searchResults: "",
+      layout: "../views/layouts/dashboard",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.dashboardSearchSubmit = async (req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const searchResults = await Note.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+      ],
+    }).where({ user: req.user.id });
+
+    res.render("dashboard/search", {
+      searchResults,
+      layout: "../views/layouts/dashboard",
     });
   } catch (error) {
     console.log(error);
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,5 +4,36 @@ const dashboardController = require("../controllers/dashboardController");
 const { isLoggedIn } = require("../middleware/checkAuth");
 
 router.get("/dashboard", isLoggedIn, dashboardController.dashboard);
+router.get(
+  "/dashboard/item/:id",
+  isLoggedIn,
+  dashboardController.dashboardViewNote
+);
+router.put(
+  "/dashboard/item/:id",
+  isLoggedIn,
+  dashboardController.dashboardUpdateNote
+);
+router.delete(
+  "/dashboard/item-delete/:id",
+  isLoggedIn,
+  dashboardController.dashboardDeleteNote
+);
+router.get("/dashboard/add", isLoggedIn, dashboardController.dashboardAddNote);
+router.post(
+  "/dashboard/add",
+  isLoggedIn,
+  dashboardController.dashboardSubmitNote
+);
+router.get(
+  "/dashboard/search",
+  isLoggedIn,
+  dashboardController.dashboardSearch
+);
+router.post(
+  "/dashboard/search",
+  isLoggedIn,
+  dashboardController.dashboardSearchSubmit
+);
 
 module.exports = router;
